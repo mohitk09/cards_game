6a310a79bb818f618720b33efc431cc266b89f05
Mohit Khotani <mohit.khotani@vimeo.com>
2024-01-18 22:49:52 +0530

    change go mod path

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-}
-
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
+
+	router.GET("/health_check", func(c *gin.Context) {
+		c.String(http.StatusOK, "App is reachable")
+	})
 
 	router.Run("localhost:8080")
 }
