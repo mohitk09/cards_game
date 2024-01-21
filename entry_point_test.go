ff54c9f5d4ecb901a5d10e6c5332455e34ac60e4
Mohit Khotani <mohit.khotani@vimeo.com>
2024-01-21 22:34:40 +0530

    create deck test cases

--- entry_point_test.go
+++ entry_point_test.go
@@ -1,35 +1,79 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/mohitk09/cards_game/types"
+	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func TestGetDeck(t *testing.T) {
+// Test create deck, a map to reduce redundancy
+func TestCreateDeck(t *testing.T) {
 	tests := []struct {
-		route        string // route path to test
-		expectedCode int    // expected HTTP status code
+		route         string
+		expectedCode  int // expected HTTP status code
+		numberOfCards int // expected number of cards in the deck
 	}{
 		{
-			route:        "/deck/something_random",
-			expectedCode: 404,
+			route:         "/deck",
+			expectedCode:  http.StatusCreated,
+			numberOfCards: 52,
+		},
+		{
+			route:         "/deck?cards=AS,7H",
+			expectedCode:  http.StatusCreated,
+			numberOfCards: 2,
 		},
 	}
 
 	db, _ := gorm.Open(sqlite.Open("cards_game_test.db"), &gorm.Config{})
-
 	app := SetupRoutes(db)
-
 	for _, test := range tests {
-		req := httptest.NewRequest("GET", test.route, nil)
-		resp, err := app.Test(req)
+		req := httptest.NewRequest("POST", test.route, nil)
+		res, err := app.Test(req, 100000)
+
+		assert.Nil(t, err)
+		assert.Equal(t, test.expectedCode, res.StatusCode)
+
+		body, err := ioutil.ReadAll(res.Body)
+		assert.Nil(t, err)
+
+		var jsonResponse map[string]json.RawMessage
+		json.Unmarshal(body, &jsonResponse)
+		assert.Contains(t, jsonResponse, "deck_id")
+		assert.Contains(t, jsonResponse, "shuffled")
+		assert.Contains(t, jsonResponse, "remaining")
+
+		var response types.CreateDeckResponse
+		err = json.Unmarshal(body, &response)
 		if err != nil {
-			fmt.Println(err)
+			assert.Equal(t, false, response.IsShuffled)
+			assert.Equal(t, test.numberOfCards, response.Remaining)
+
 		}
-		fmt.Println(resp)
 	}
+}
+
+func TestCreateDeckMalformedCase(t *testing.T) {
+	db, _ := gorm.Open(sqlite.Open("cards_game_test.db"), &gorm.Config{})
+	app := SetupRoutes(db)
+	req := httptest.NewRequest("POST", "/deck?cards=YZ", nil)
+	res, _ := app.Test(req)
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+
+}
+
+func TestOpenDeckRecordNotFound(t *testing.T) {
+	db, _ := gorm.Open(sqlite.Open("cards_game_test.db"), &gorm.Config{})
+	app := SetupRoutes(db)
+	req := httptest.NewRequest("GET", "/deck/something_random", nil)
+	res, _ := app.Test(req)
+	assert.Equal(t, http.StatusNotFound, res.StatusCode)
+
 }
